ec2a4452839fbe6ced51b8251484071e2e6e292f
Jhoan Ramirez <ramirezospinajhoanesteban@gmail.com>
2026-04-08 07:11:09 -0500

    Revert "correccion de errores al cargar los datos"

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api"
+
+baseURL:"http://127.0.0.1:8000/api"
+
 })
 
 export default api
--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,10 +1,4 @@
-import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
-
-const api = axios.create({
-    baseURL: API_URL
-});
+const API_URL = "http://127.0.0.1:8000/api"
 
 const getHeaders = (isPost = false) => {
     const headers = {
